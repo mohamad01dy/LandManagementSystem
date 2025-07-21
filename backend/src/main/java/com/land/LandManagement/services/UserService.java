ececdcf374fdec121514580869fe4af795a3656f
Mouhamad El Dayeh <dayeh800@gmail.com>
2025-07-21 11:33:43 +0300

    user authorization fixed

--- backend/src/main/java/com/land/LandManagement/services/UserService.java
+++ backend/src/main/java/com/land/LandManagement/services/UserService.java
@@ -5,6 +5,8 @@ import com.land.LandManagement.domain.tables.User;
 import com.land.LandManagement.repositories.LandRepository;
 import com.land.LandManagement.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -25,11 +27,34 @@ public class UserService {
     }
 
     public User getUser(Integer id) {
+        // Get email from the token
+        String authenticatedEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // Fetch user to be returned from DB
+        User targetUser = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        // Check if the authenticated user is the owner
+        if (!targetUser.getEmail().equals(authenticatedEmail)) {
+            throw new AccessDeniedException("You are not allowed to update this user.");
+        }
         return userRepository.getReferenceById(id);
     }
 
     public void updateUser(User user) {
-        getUser(user.getId());
+
+        // Get email from the token
+        String authenticatedEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // Fetch user to be updated from DB
+        User targetUser = userRepository.findById(user.getId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        // Check if the authenticated user is the owner
+        if (!targetUser.getEmail().equals(authenticatedEmail)) {
+            throw new AccessDeniedException("You are not allowed to update this user.");
+        }
+
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
